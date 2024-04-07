97828694dc6e2b5951bfc3e065e00d0cd4a69284
Hide <bluehawaii0918@gmail.com>
2024-04-07 23:00:37 +0900

    xdc3-add

--- routes/rpcCheck.js
+++ routes/rpcCheck.js
@@ -1,31 +1,8 @@
-const Xdc3 = require("xdc3");
-
-const rpc_targets = [
-  ["https://erpc.apothem.network","apothem"],
-  ["https://earpc.apothem.network","apothem"],
-  ["https://erpc.xinfin.network","mainnet"],
-  ["https://earpc.xinfin.network","mainnet"]
-];
-
-const ws_targets = [
-  ["wss://ws.apothem.network/ws","apothem"],
-  ["wss://ws.xinfin.network","mainnet"],
-  ["wss://ews.xinfin.network","mainnet"],
-  ["wss://aws.xinfin.network","mainnet"],
-  ["wss://eaws.xinfin.network","mainnet"]
-];
-
-const plitoken = {
-    "mainnet": "0xFf7412Ea7C8445C46a8254dFB557Ac1E48094391",
-    "apothem": "0x33f4212b027E22aF7e6BA21Fc572843C0D701CD1"
-  };
-  
-
-
-
-
-
 var express = require('express');
+
+const Web3 = require('xdc3');
+let web3 = new Web3('ws://localhost:8546');
+
 var router = express.Router();
 
 /* GET home page. */
