10251c5eccc3149355da187f257c5fb43bdfc0d5
Hide <bluehawaii0918@gmail.com>
2024-04-11 00:27:16 +0900

    テーブル作成

--- public/javascripts/network.js
+++ public/javascripts/network.js
@@ -1,37 +1,59 @@
 $(function () {
   // JSONデータの取得先
   const url = "./data/resultList.json";
-  let html = "";
-  let htmlParts;
-  $.getJSON(url, (data) => {
-    htmlParts = data.DATE + "<br>";
 
+  $.getJSON(url, (data) => {
+    let mainRpcLen = data["MAINNET"]["RPC"].length;
+    let mainWssLen = data["MAINNET"]["WSS"].length;
+    let aptmRpcLen = data["APOTHEM"]["RPC"].length;
+    let aptmWssLen = data["APOTHEM"]["WSS"].length;
+    let netRowSpan = [mainRpcLen+mainWssLen, aptmRpcLen+aptmWssLen];
+    let typeRowSpan = [mainRpcLen, mainWssLen, aptmRpcLen, aptmWssLen];
+
+    const table1 = $("<table border='1'><tbody>");
+    $("<tr><td colspan='7'>作成日付：" + data.DATE + "</td></tr>").appendTo(table1);
+    $("<tr><td>net</td><td>type</td><td>url</td><td>version</td><td>block</td><td>gas(Gwei)</td><td>0xPrefix</td></tr>").appendTo(table1);
+
+    let netCnt=0;
+    let typeCnt=0;
+    let itemCnt=0;
+    let html = "";
     // JSONの各要素に対して
     for(let network in data) {
       if(network == "DATE"){
         break;
       }
-      htmlParts += "<strong>" + network + "</strong><br>";
+      html = "<tr><td rowspan="+netRowSpan[netCnt]+">"+network+"</td>";
       for(let type in data[network]){
-        htmlParts += "<strong>" + type + "</strong><br>";
+        if(typeCnt!=0){
+          html += "<tr>";
+        }
+        html += "<td rowspan="+typeRowSpan[typeCnt]+">"+type+"</td>";
         for(let item in data[network][type]){
           let url = data[network][type][item]["url"];
           let ver = data[network][type][item]["ver"];
           let blc = data[network][type][item]["blc"];
           let gas = data[network][type][item]["gas"];
           let pfx = data[network][type][item]["pfx"];
-          htmlParts += url + "," + ver + "," + blc+ "," + gas + "," + pfx +"<br>";
+          if(item!=0){
+            html += "<tr>";
+          }
+          html += "<td>"+url+"</td><td>"+ver+"</td><td>"+blc+"</td><td>"+gas+"</td><td>"+pfx+"</td></tr>";
+          $(html).appendTo(table1);
+          html = "";
+          itemCnt++;
         }
+        itemCnt=0;
+        typeCnt++;
       }
-      console.log(htmlParts);
-      // 先述の変数の中に、出来上がったHTMLを格納
-      html += htmlParts;
-      htmlParts = "";
+      typeCnt=0;
+      netCnt++; 
     }
     // 指定場所へHTML出力
-    $("#rpc-list").html(html);
+    $("</tbody></table>").appendTo(table1);
+    $("#networkList").append(table1);
   }, function(){
-    $("#rpc-list").html("<p>データの取得に失敗しました。</p>");
+    $("#networkList").html("<p>データの取得に失敗しました。</p>");
   });
 });
   
