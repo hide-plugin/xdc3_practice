1be2437ad0d19d95c964db80698f4e3dd4e812bb
Hide <bluehawaii0918@gmail.com>
2024-04-11 14:27:07 +0900

    networkList更新機能追加

--- public/javascripts/network.js
+++ public/javascripts/network.js
@@ -1,59 +1,83 @@
+
 $(function () {
   // JSONデータの取得先
   const url = "./data/resultList.json";
 
-  $.getJSON(url, (data) => {
-    let mainRpcLen = data["MAINNET"]["RPC"].length;
-    let mainWssLen = data["MAINNET"]["WSS"].length;
-    let aptmRpcLen = data["APOTHEM"]["RPC"].length;
-    let aptmWssLen = data["APOTHEM"]["WSS"].length;
-    let netRowSpan = [mainRpcLen+mainWssLen, aptmRpcLen+aptmWssLen];
-    let typeRowSpan = [mainRpcLen, mainWssLen, aptmRpcLen, aptmWssLen];
-
-    const table1 = $("<table border='1'><tbody>");
-    $("<tr><td colspan='7'>作成日付：" + data.DATE + "</td></tr>").appendTo(table1);
-    $("<tr><td>net</td><td>type</td><td>url</td><td>version</td><td>block</td><td>gas(Gwei)</td><td>0xPrefix</td></tr>").appendTo(table1);
-
-    let netCnt=0;
-    let typeCnt=0;
-    let itemCnt=0;
-    let html = "";
-    // JSONの各要素に対して
-    for(let network in data) {
-      if(network == "DATE"){
-        break;
-      }
-      html = "<tr><td rowspan="+netRowSpan[netCnt]+">"+network+"</td>";
-      for(let type in data[network]){
-        if(typeCnt!=0){
-          html += "<tr>";
+  async function main(){
+    $.getJSON(url, (data) => {
+      let mainRpcLen = data["MAINNET"]["RPC"].length;
+      let mainWssLen = data["MAINNET"]["WSS"].length;
+      let aptmRpcLen = data["APOTHEM"]["RPC"].length;
+      let aptmWssLen = data["APOTHEM"]["WSS"].length;
+      let netRowSpan = [mainRpcLen+mainWssLen, aptmRpcLen+aptmWssLen];
+      let typeRowSpan = [mainRpcLen, mainWssLen, aptmRpcLen, aptmWssLen];
+  
+      const table1 = $("<table border='1'><tbody>");
+      $("<tr><td colspan='7'>作成日付：" + data.DATE + "</td></tr>").appendTo(table1);
+      $("<tr><td>net</td><td>type</td><td>url</td><td>version</td><td>block</td><td>gas(Gwei)</td><td>0xPrefix</td></tr>").appendTo(table1);
+  
+      let netCnt=0;
+      let typeCnt=0;
+      let itemCnt=0;
+      let html = "";
+      // JSONの各要素に対して
+      for(let network in data) {
+        if(network == "DATE"){
+          break;
         }
-        html += "<td rowspan="+typeRowSpan[typeCnt]+">"+type+"</td>";
-        for(let item in data[network][type]){
-          let url = data[network][type][item]["url"];
-          let ver = data[network][type][item]["ver"];
-          let blc = data[network][type][item]["blc"];
-          let gas = data[network][type][item]["gas"];
-          let pfx = data[network][type][item]["pfx"];
-          if(item!=0){
+        html = "<tr><td rowspan="+netRowSpan[netCnt]+">"+network+"</td>";
+        for(let type in data[network]){
+          if(typeCnt!=0){
             html += "<tr>";
           }
-          html += "<td>"+url+"</td><td>"+ver+"</td><td>"+blc+"</td><td>"+gas+"</td><td>"+pfx+"</td></tr>";
-          $(html).appendTo(table1);
-          html = "";
-          itemCnt++;
+          html += "<td rowspan="+typeRowSpan[typeCnt]+">"+type+"</td>";
+          for(let item in data[network][type]){
+            let url = data[network][type][item]["url"];
+            let ver = data[network][type][item]["ver"];
+            let blc = data[network][type][item]["blc"];
+            let gas = data[network][type][item]["gas"];
+            let pfx = data[network][type][item]["pfx"];
+            if(item!=0){
+              html += "<tr>";
+            }
+            html += "<td>"+url+"</td><td>"+ver+"</td><td>"+blc+"</td><td>"+gas+"</td><td>"+pfx+"</td></tr>";
+            $(html).appendTo(table1);
+            html = "";
+            itemCnt++;
+          }
+          itemCnt=0;
+          typeCnt++;
         }
-        itemCnt=0;
-        typeCnt++;
+        typeCnt=0;
+        netCnt++; 
       }
-      typeCnt=0;
-      netCnt++; 
-    }
-    // 指定場所へHTML出力
-    $("</tbody></table>").appendTo(table1);
-    $("#networkList").append(table1);
-  }, function(){
-    $("#networkList").html("<p>データの取得に失敗しました。</p>");
+
+      // 指定場所へHTML出力
+      $("</tbody></table>").appendTo(table1);
+      $("#networkList").html(table1);
+    }, function(){
+      $("#networkList").html("<p>データの取得に失敗しました。</p>");
+    });
+  }
+
+  main();
+
+  // 更新ボタンイベント
+  $('#button').click(function () {
+    $("#networkList").html("リスト更新中");
+
+    $.ajax({
+      url: "/createList", 
+      type: "GET",
+      data: "",
+      success: function(response) {
+        main();
+        // API呼び出し成功時の処理
+      },error: function(xhr, status, error) {
+        // API呼び出し失敗時の処理
+        $("#networkList").html("リスト更新に失敗しました。");
+      }
+    });
   });
 });
-  
+
