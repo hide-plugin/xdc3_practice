80be1b69c8441250aeda2e9b3c7bcf440e83ecba
Hide <bluehawaii0918@gmail.com>
2024-05-21 23:21:37 +0900

    不要ファイル削除

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // router
 app.use('/', require('./routes/index'));
 app.use('/networkStatus', require('./routes/networkStatus'));
-app.use('/getBalance', require('./routes/getBalance'));
-app.use('/searchBalance', require('./routes/searchBalance'));
+app.use('/walletBalance', require('./routes/walletBalance'));
 app.use('/srxStatus', require('./routes/srxStatus'));
 
 // catch 404 and forward to error handler
